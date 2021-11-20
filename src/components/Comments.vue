e8ad95e4a4193a21ab74129c9dd00dac17267132
“rivnoj” <rivnoj@gmail.com>
2021-11-20 17:56:45 -0300

    Transformando a aplicação para fazer uso de mais de um component

--- src/components/Comments.vue
+++ src/components/Comments.vue
@@ -2,31 +2,10 @@
   <div class="container">
     <h1>Comentários</h1>
     <hr />
-    <div class="form-todo form-group">
-      <p>
-        <input
-          placeholder="nome"
-          type="text"
-          name="author"
-          class="form-control"
-          v-model="name"
-        />
-      </p>
-      <p>
-        <textarea
-          placeholder="comentário"
-          name="message"
-          class="form-control"
-          v-model="message"
-        ></textarea>
-      </p>
-      <button v-on:click="addComment" type="submit" class="btn btn-primary">
-        Comentar
-      </button>
-    </div>
-    <br />
+    <FormTodo v-on:add-todo="addComment"></FormTodo>
     <div class="list-group">
-      <div class="list-group-item" v-for="(comment, index) in allComments" :key="index">
+      <p v-if="comments.length <= 0">Sem comentários...</p>
+      <div class="list-group-item" v-for="(comment, index) in allComments" v-bind:key="index">
         <span class="comment__author"
           >Autor: <strong>{{ comment.name }}</strong></span
         >
@@ -43,31 +22,25 @@
 </template>
 
 <script>
+import FormTodo from './FormTodo'
+
 export default {
+  components: {
+    FormTodo
+  },
   data() {
     return {
-      comments: [],
-      name: "",
-      message: "",
+      comments: []
     };
   },
   methods: {
-    addComment() {
-      if (this.message.trim() == "") {
-        return;
-      }
-
-      this.comments.push({
-        name: this.name,
-        message: this.message,
-      });
-
-      this.name = "";
-      this.message = "";
-    },
     removeComment(index) {
       this.comments.splice(index, 1);
     },
+
+    addComment(comment) {
+      this.comments.push(comment);
+    }
   },
   computed: {
     allComments() {
